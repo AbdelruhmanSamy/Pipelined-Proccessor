343aaec5a15d09d5bce9e23866610762de3c6b2b
AbdelruhmanSamy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-12-23 10:42:52 +0200

    🐛 (jump_unit): Jump_unit compiled successfully

--- units/jump_unit.vhd
+++ units/jump_unit.vhd
@@ -13,9 +13,7 @@ END jump_unit;
 ARCHITECTURE rtl OF jump_unit IS
 
 BEGIN
-    JUMP <= '1' WHEN (JC AND Flags(2) = '1') OR (JN AND Flags(1) = '1') OR (JZ AND Flags(0) = '1') ELSE
+    JUMP <= '1' WHEN (JC = '1' AND Flags(2) = '1') OR (JN = '1' AND Flags(1) = '1') OR (JZ = '1' AND Flags(0) = '1') ELSE
         '0';
 
 END rtl;
-
-END ARCHITECTURE;
